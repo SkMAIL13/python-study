7d0efec65ed8099451326fe0646dfcc876b0e442
Kamran Ismailov <iskama98@mail.ru>
2024-07-31 20:57:21 +0300

    cube

---
+++ course1/1input-output/cube.py
@@ -0,0 +1,6 @@
+capacity = int(input())
+capacity_summ = capacity * capacity * capacity
+square = 6 * capacity * capacity
+
+print("Объем =", capacity_summ)
+print("Площадь полной поверхности =", square)
