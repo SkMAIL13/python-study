5ac9bbb864bba087c6eca0f853367df80d95dca4
Kamran Ismailov <iskama98@mail.ru>
2024-08-01 22:37:35 +0300

    purchase price

---
+++ course1/1input-output/purchase_price.py
@@ -0,0 +1,6 @@
+price_monitor = int(input())
+price_system_unit = int(input())
+price_keyboard = int(input())
+price_mice = int(input())
+
+print(3 * (price_monitor + price_system_unit + price_keyboard + price_mice))
